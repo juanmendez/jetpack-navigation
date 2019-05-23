752fe2d583752182e941fec3caa7f3f20a4f6aef
Juan Mendez <contact@juanmendez.info>
2019-05-23 11:13:27 -0500

    navigating through actions and passing arguments

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -24,6 +24,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 
@@ -45,25 +46,11 @@ class HomeFragment : Fragment() {
 
         val button = view.findViewById<Button>(R.id.navigate_destination_button)
 
-        val options = navOptions {
-            anim {
-                enter = R.anim.slide_in_right
-                exit = R.anim.slide_out_left
-                popEnter = R.anim.slide_in_left
-                popExit = R.anim.slide_out_right
-            }
+        button?.setOnClickListener{
+            val flowStepNumberArg = 1
+            val action = HomeFragmentDirections.nextAction(flowStepNumberArg)
+            findNavController().navigate(action)
         }
-
-        button?.setOnClickListener {
-            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-        }
-        //TODO END STEP 6
-
-        //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
-        //        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
-        //                Navigation.createNavigateOnClickListener(R.id.next_action, null)
-        //        )
-        //TODO END STEP 7.2
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
